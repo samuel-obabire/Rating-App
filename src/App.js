d228931695045039cb858a3d6fc8d4c991504848
Samuel <samuelobabire6@gmail.com>
2022-05-25 21:02:00 +0100

    adds header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css"
 
 import FeedbackProvider from "./context/FeedbackContext"
+import Header from "./components/header/Header"
 import RatingSelect from "./components/rating-select/RatingSelect"
 import RatingReview from "./components/rating-review/RatingReview"
 import FeedbackList from "./components/feedbackList/FeedbackList"
@@ -9,6 +10,7 @@ const App = () => {
   return (
     <div className="container">
       <FeedbackProvider>
+        <Header />
         <RatingSelect />
         <RatingReview />
         <FeedbackList />
---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Feedback UI</h1>
+    </header>
+  )
+}
+
+export default Header
